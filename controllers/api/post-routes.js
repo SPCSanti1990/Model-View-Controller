dea7ccedc400be3876b49d6466751bafcbec14df
SPCSanti1990 <81482804+SPCSanti1990@users.noreply.github.com>
2024-01-15 16:58:32 -0500

    comments added to opperations layout for post-routes, user-routes, and comment-routes

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {User, Post, Comment} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Get all posts
+
+// Get a single post
+
+// Create a post
+
+// Update a post
+
+// Delete a post
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const {User, Post, Comment} = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// Get all users
+
+// Get a specific user
+
+// Create a user
+
+// Login
+
+// Logout
